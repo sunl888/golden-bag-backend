72d789646fce8c9dca2c9dfd9c4abbde6b7ba6f0
wq1019 <2013855675@qq.com>
2018-07-23 16:55:34 +0800

    添加測試配置文件

--- src/test/java/com/zmdev/goldenbag/GoldenBagApplicationTests.java
+++ src/test/java/com/zmdev/goldenbag/GoldenBagApplicationTests.java
@@ -4,15 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-// Enable JMX so we can test the MBeans (you can't do this in a properties file)
-@TestPropertySource(properties = {"spring.jmx.enabled:true",
-        "spring.datasource.jmx-enabled:true"})
-@ActiveProfiles("test_db")
+@ActiveProfiles("testing")
 public class GoldenBagApplicationTests {
 
     @Test
