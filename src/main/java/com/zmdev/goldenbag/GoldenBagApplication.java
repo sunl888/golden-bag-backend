19b20369e3ecd7e91305273a30dfdaaf352e8655
wq1019 <2013855675@qq.com>
2018-07-24 21:18:41 +0800

    update

--- src/main/java/com/zmdev/goldenbag/GoldenBagApplication.java
+++ src/main/java/com/zmdev/goldenbag/GoldenBagApplication.java
@@ -2,7 +2,12 @@ package com.zmdev.goldenbag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing
