b8aa446e93fbc2b04e7a4c67c92b65635b0d77c7
taoyu <1255803549@qq.com>
2018-07-24 19:39:21 +0800

    单点登录

--- src/main/java/com/zmdev/goldenbag/GoldenBagApplication.java
+++ src/main/java/com/zmdev/goldenbag/GoldenBagApplication.java
@@ -2,11 +2,13 @@ package com.zmdev.goldenbag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zmdev.goldenbag", "com.zmdev.fatesdk"})
 public class GoldenBagApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoldenBagApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenBagApplication.class, args);
+    }
 }
