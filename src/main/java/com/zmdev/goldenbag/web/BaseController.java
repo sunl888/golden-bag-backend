e00cf6a4a3a2ac4bbdf75b0a9681e6005e58416c
tw1997 <t888y@qq.com>
2018-07-24 21:19:07 +0800

    /api

--- src/main/java/com/zmdev/goldenbag/web/BaseController.java
+++ src/main/java/com/zmdev/goldenbag/web/BaseController.java
@@ -1,7 +1,11 @@
 package com.zmdev.goldenbag.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class BaseController {
 }
