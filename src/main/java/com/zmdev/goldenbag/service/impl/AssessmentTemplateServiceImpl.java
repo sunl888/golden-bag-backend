759cfe62a4aa4ba3e50ebbbf517845a0f816ff30
tw1997 <t888y@qq.com>
2018-07-25 14:34:57 +0800

    使用 ModelNotFoundException

--- src/main/java/com/zmdev/goldenbag/service/impl/AssessmentTemplateServiceImpl.java
+++ src/main/java/com/zmdev/goldenbag/service/impl/AssessmentTemplateServiceImpl.java
@@ -1,7 +1,7 @@
 package com.zmdev.goldenbag.service.impl;
 
 import com.zmdev.goldenbag.domain.*;
-import com.zmdev.goldenbag.exception.ServiceException;
+import com.zmdev.goldenbag.exception.ModelNotFoundException;
 import com.zmdev.goldenbag.service.AssessmentTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +26,7 @@ public class AssessmentTemplateServiceImpl extends BaseServiceImpl<AssessmentTem
     public void saveProject(Long templateId, AssessmentProject project) {
         AssessmentTemplate template = repository.findById(templateId).orElse(null);
         if (template == null) {
-            throw new ServiceException("模版不存在");
+            throw new ModelNotFoundException("模版不存在");
         }
         project.setAssessmentTemplate(template);
         assessmentProjectRepository.save(project);
@@ -40,7 +40,7 @@ public class AssessmentTemplateServiceImpl extends BaseServiceImpl<AssessmentTem
     public void saveProjectItem(Long projectId, AssessmentProjectItem projectItem) {
         AssessmentProject project = assessmentProjectRepository.findById(projectId).orElse(null);
         if (project == null) {
-            throw new ServiceException("考核项目标准不存在");
+            throw new ModelNotFoundException("考核项目标准不存在");
         }
         projectItem.setAssessmentProject(project);
         assessmentProjectItemRepository.save(projectItem);
