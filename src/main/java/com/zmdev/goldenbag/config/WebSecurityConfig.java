0f2d532748a56f3f0b4734cec986c724faac1fca
taoyu <1255803549@qq.com>
2018-08-03 09:25:35 +0800

    update

--- src/main/java/com/zmdev/goldenbag/config/WebSecurityConfig.java
+++ src/main/java/com/zmdev/goldenbag/config/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
         // 权限验证拦截器
         registry.addInterceptor(new PermissionInterceptor(userService, auth, permissionService)).addPathPatterns("/**")
                 // 单个导出考核记录接口不做权限验证 在此接口内部用代码做验证
-                .excludePathPatterns("assessments/export/*", "templates/types", "permissions/menus", "templates/get_template", "/setup/*", "/error", "/login", "/error", "/fate/callback", "/fate/logout");
+                .excludePathPatterns("/", "assessments/export/*", "templates/types", "permissions/menus", "templates/get_template", "/setup/*", "/error", "/login", "/error", "/fate/callback", "/fate/logout");
         // 模板编辑拦截器
         registry.addInterceptor(templateInterceptor).addPathPatterns("/templates/**");
     }
